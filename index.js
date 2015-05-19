d172067f459aa57482810300d3a2e5bf5df5ef49
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-05-19 18:03:16 +0300

    added server stub

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3000, '127.0.0.1');
+
+console.log('Server is running');
